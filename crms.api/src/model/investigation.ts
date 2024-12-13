e972c699737c54a660c5d4463987db59e7f1c001
Joya-Banik <138750788+Joya-Banik@users.noreply.github.com>
2024-12-13 20:38:56 +0600

    first commit

---
+++ crms.api/src/model/investigation.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, JoinColumn, JoinTable, OneToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Users } from "./user";
+
+@Entity()
+export class Investigation{
+    @PrimaryColumn()
+    id: string;
+    
+    @OneToOne(() => Users)
+    @JoinColumn()
+    user : Users
+
+    @Column()
+    rank: string
+}
